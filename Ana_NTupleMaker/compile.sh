07f143a97dd576e705195931e96eff1b0cac525e
Samuel Ross Meehan <smeehan12@gmail.com>
2017-04-28 21:19:24 +0200

    changes

--- Ana_NTupleMaker/compile.sh
+++
@@ -1,9 +0,0 @@
-export FJINSTALL=/Users/meehan/work/TelescopingJets/TelescopingJets/fastjet-3.2.1
-
-echo
-echo "Compiling with : "
-echo "$ROOTSYS    : "${ROOTSYS}
-echo "$FJINSTALL : "${FJINSTALL}
-echo
-
-g++ -o NTupler.exe NTupler.cc TelescopingJets.cc -I$ROOTSYS/include -I$FJINSTALL/include  -rpath $ROOTSYS/lib `$ROOTSYS/bin/root-config --glibs` `$FJINSTALL/fastjet-config --cxxflags --libs --plugins` -lNsubjettiness -lEnergyCorrelator   -std=c++11
